5203f4d14528c8edf7ddc7ebfbe0bc180f63f164
Andrea Iglio <andrea.iglio@gmail.com>
2020-12-16 13:42:33 +0100

    AddTask functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,15 +5,19 @@ let app = new Vue({
   el: "#compiti",
 
   data: {
-    tasks: ["Compra pane", "Mangia patate", "Vendi ortaggi"],
-    icon: "fas fa-minus-circle",
+      initValue: '',
+      tasks: ["Compra pane", "Mangia patate", "Vendi ortaggi"],
+      icon: "fas fa-minus-circle",
   },
 
   methods: {
 
     removeTask : function () {
         this.tasks.pop();
-      }
+      },
 
+    addTask : function (){
+        this.tasks.push(this.initValue);
+    },
   },
 });
