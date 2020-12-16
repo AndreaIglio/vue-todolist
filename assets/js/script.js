4c813dde18cd2c7684e489e9bf3e9d728e035dc5
Andrea Iglio <andrea.iglio@gmail.com>
2020-12-16 12:45:33 +0100

    Implemented VueJs instance
    Implemented default li + icon using VueJs instance

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,13 @@
-// Codice viene eseguito solo quando documento e' carico
-$(function () {
- 
+
+
+
+let app = new Vue({
+  el: "#compiti",
+
+  data: {
+    tasks: ["Compra pane", "Mangia patate", "Vendi ortaggi"],
+    icon: "fas fa-minus-circle",
+  },
+
+  methods: {},
 });
