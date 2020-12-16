a159cbc74706d955bba28da18c7ae3f48d49c459
Andrea Iglio <andrea.iglio@gmail.com>
2020-12-16 15:22:11 +0100

    Implemented v-on:keydown.enter on input

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,28 +4,23 @@
 let app = new Vue({
   el: "#compiti",
   data: {
-      initValue: '',
-      tasks: ["Compra pane", "Mangia patate", "Vendi ortaggi"],
-      icon: "fas fa-minus-circle",
+    initValue: "",
+    tasks: ["Compra pane", "Mangia patate", "Vendi ortaggi"],
+    icon: "fas fa-minus-circle",
   },
 
-
-
   methods: {
+    removeTask: function (index) {
+      this.tasks.splice(index, 1);
+    },
 
-    removeTask : function (index) {
-        
-        this.tasks.splice(index,1)
-
-   },
-   
-   addTask : function (){
-
-        (this.initValue.length > 3) ? this.tasks.push(this.initValue): alert('The task need to have at least 4 characters');
-        this.initValue = "";
+    addTask: function () {
+      this.initValue.length > 3
+        ? this.tasks.push(this.initValue)
+        : alert("The task need to have at least 4 characters");
+      this.initValue = "";
     },
   },
 
-//chiedi come poter resettare il valore di initValue usando un "Hook"
-
+  //chiedi come poter resettare il valore di initValue usando un "Hook"
 });
